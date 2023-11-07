71d58c614012434198a736a652d69a61c422a0f7
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-11-07 19:37:15 +0200

    basic route

--- index.js
+++ index.js
@@ -4,13 +4,34 @@ const port = 8000;
 
 const app = express();
 
-app.get('/hello', (req, res) => {
-  try {
-    res.send('hi!');
-  } catch (error) {
-    console.log(error);
-  }
+app.all('/hello', (req, res, next) => {
+  console.log('all');
+  next();
 });
+
+const cb = (req, res, next) => {
+  console.log('CALLBACK');
+  next();
+};
+
+app
+  .route('/user')
+  .get('/hel?lo', [cb, cb, cb], (req, res) => {
+    try {
+      res.send('hi!');
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  .post('/hel?lo', [cb, cb, cb], (req, res) => {
+    try {
+      res.send('hi post !');
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
 
 app.listen(port, () => {
   console.log(`Server is on http://localhost:${port}`);
