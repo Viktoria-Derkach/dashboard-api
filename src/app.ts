0a84beb26c3a43fd3f30de27184a445f43bd26bd
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-11-08 21:24:35 +0200

    logger

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
-import express, { Express, Response, NextFunction } from 'express';
+import express, { Express } from 'express';
 import { userRouter } from './users/users';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.server';
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  logger: LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   useRoutes() {
@@ -19,6 +22,6 @@ export class App {
   public async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port);
-    console.log(`Server is on http://localhost:${this.port}`);
+    this.logger.log(`Server is on http://localhost:${this.port}`);
   }
 }
